70482ec17c1184a3f979656bbd2276d58ba09987
alinaincodeland <alinacuznetov@gmail.com>
2023-02-22 14:50:29 +0100

    Fixed the warnings.

---
+++ client/src/pages/TeacherLessons.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LessonCard from "../components/LessonCard";
+
+const TeacherLessons = () => {
+  return (
+    <div>
+      <LessonCard />
+    </div>
+  );
+};
+
+export default TeacherLessons;
