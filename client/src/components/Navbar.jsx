c039a5cfedb6d6da716e80c806afeca3e934bd8a
ju8ko <julia.x.kobzar@gmail.com>
2023-02-22 21:02:18 +0100

    Use NavLink Instead of Link. Add teacher profile navigation.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import {
   MDBContainer,
   MDBNavbar,
@@ -8,51 +8,67 @@ import {
   MDBNavbarNav,
   MDBNavbarItem,
   MDBNavbarLink,
-  MDBCollapse, MDBBtn,
-  MDBIcon
-} from 'mdb-react-ui-kit';
+  MDBCollapse,
+  MDBBtn,
+  MDBIcon,
+} from "mdb-react-ui-kit";
 
-export default function Navbar({theme}) {
+export default function Navbar({ theme }) {
   const [showNav, setShowNav] = useState(false);
 
   return (
-    <MDBNavbar expand='lg' light bgColor='light'>
+    <MDBNavbar expand="lg" light bgColor="light">
       <MDBContainer fluid>
-        <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
+        <MDBNavbarBrand href="#">Navbar</MDBNavbarBrand>
         <MDBNavbarToggler
-          type='button'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          type="button"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
           onClick={() => setShowNav(!showNav)}
         >
-          <MDBIcon icon='bars' fas />
+          <MDBIcon icon="bars" fas />
         </MDBNavbarToggler>
         <MDBCollapse navbar show={showNav}>
-          <MDBNavbarNav className='d-flex justify-content-end gap-4 '>
+          <MDBNavbarNav className="d-flex justify-content-end gap-4 ">
             <MDBNavbarItem>
-            <Link to='/'>
-              <MDBNavbarLink active aria-current='page' href='#'>
-                Home
+              <NavLink to="/">
+                {({ isActive }) => (
+                  <MDBNavbarLink active={isActive} aria-current="page">
+                    Home
+                  </MDBNavbarLink>
+                )}
+              </NavLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <NavLink to="/teacher/profile">
+                {({ isActive }) => (
+                  <MDBNavbarLink
+                    active={isActive}
+                    className={theme === "home" && " d-none "}
+                  >
+                    Profile
+                  </MDBNavbarLink>
+                )}
+              </NavLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink
+                href="#"
+                className={theme === "home" && " d-none "}
+              >
+                Lessons
               </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem className={theme === "home" && " d-none "}>
+              <MDBNavbarLink href="#">Schedule</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <Link to="/login">
+                <MDBBtn outline color="success" className="me-2" type="button">
+                  Log In/Out
+                </MDBBtn>
               </Link>
-            </MDBNavbarItem>          
-              <MDBNavbarItem>
-              <MDBNavbarLink href='#' className={theme==='home'&&' d-none '} >Profile</MDBNavbarLink>
             </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#' className={theme==='home'&&' d-none '} >Lessons</MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem className={theme==='home'&&' d-none '}>
-              <MDBNavbarLink href='#'  >Schedule</MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <Link to='/login'>
-            <MDBBtn outline color="success" className='me-2' type='button'>
-          Log In/Out
-          </MDBBtn>
-          </Link>
-          </MDBNavbarItem>
-        
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBContainer>
