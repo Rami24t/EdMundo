c7b72225b9cbdba2fcca7063baa5c0783e43378c
RamS24 <AlSaadi.Rami24@gmail.com>
2023-02-23 16:10:14 +0100

    add backend for registering and logging in users

--- server/src/models/Teacher.js
+++ server/src/models/Teacher.js
@@ -8,23 +8,21 @@ const TeacherSchema = new Schema({
     email: {
         type: String,
         required: true,
-        unique: true
     },
-    username: {
-        type: String,
-        //        required: true
-    },
+    // username: {
+    //     type: String,
+    //     unique: true,
+    //     //        required: true
+    // },
     password: {
         type: String,
         required: true
     },
     phone: {
         type: String,
-        required: true
     },
     address: {
         type: String,
-        required: true
     },
     role: {
         type: String,
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,19 +1,20 @@
 import express from 'express'
-import { getUserPublic, register, login, emailConfirm, forgotPass, changePass, logout, updateCover, updateProfile } from '../controllers/userController.js'
+import { getUserPublic, register, login, emailConfirm, forgotPass, changePass, logout, updateCover, updateProfile } from '../controllers/user.js'
 import auth from '../middleware/auth.js'
 import multerMiddleware from '../config/multer-cloudinary.js'
 const router = express.Router()
 import { check } from 'express-validator'
 
 router.post('/register', [
-    check('username', 'Username is required').notEmpty(),
     check('email', 'Please include a valid email').isEmail().trim().escape().normalizeEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 ], register)
 router.post('/emailconfirm', emailConfirm)
+
 router.post('/login', [
-    check('*').notEmpty().trim().escape()
+    // check('*').notEmpty().trim().escape()
 ], login)
+
 router.get('/logout', logout)
 
 router.get('/public/:id', auth, getUserPublic)
