18e724893ed0026dacb501000da27da2bc174ff5
alinaincodeland <alinacuznetov@gmail.com>
2023-02-23 13:33:26 +0100

    Edited Teacher lessons page and Lesson card component.

--- client/src/components/LessonCard.jsx
+++ client/src/components/LessonCard.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardHeader,
+  MDBCardFooter,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import { CiEdit } from "react-icons/ci";
+import { TiDeleteOutline } from "react-icons/ti";
 
 const LessonCard = () => {
-  return <div>LessonCard</div>;
+  return (
+    <>
+      <MDBCard>
+        <MDBCardHeader className="d-flex flex-row justify-content-between align-items-center">
+          <h4>German</h4>
+          <div className="buttons">
+            <MDBBtn href="#">
+              <CiEdit />
+            </MDBBtn>
+            <MDBBtn href="#">
+              <TiDeleteOutline />
+            </MDBBtn>
+          </div>
+        </MDBCardHeader>
+        <MDBCardBody>
+          <MDBCardTitle>Durch die Sprachen</MDBCardTitle>
+          <MDBCardText>
+            <div className="d-flex flex-row justify-content-between my-3">
+              <h2>My Lessons</h2>
+              <button>Create a new lesson</button>
+            </div>
+          </MDBCardText>
+        </MDBCardBody>
+        <MDBCardFooter className="text-muted">2 days ago</MDBCardFooter>
+      </MDBCard>
+    </>
+  );
 };
 
 export default LessonCard;
--- client/src/pages/TeacherLessons.jsx
+++ client/src/pages/TeacherLessons.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 import LessonCard from "../components/LessonCard";
+import Footer from "../components/Footer";
 
 const TeacherLessons = () => {
   return (
     <div>
+      <Navbar />
+      <div className="d-flex flex-row justify-content-between my-3">
+        <h2>My Lessons</h2>
+        <button>Create a new lesson</button>
+      </div>
       <LessonCard />
+      <Footer />
     </div>
   );
 };
