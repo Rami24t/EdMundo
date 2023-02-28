7e2381f640547afa4c411d6576f63e3ccb617dec
Rami <alsaadi.rami24@gmail.com>
2023-02-28 19:38:53 +0100

    fixed School model

--- server/src/models/School.js
+++ server/src/models/School.js
@@ -2,69 +2,79 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const PeriodSchema = new Schema({
-    number: {
-        enum: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-        type: Number,
-        required: true
-    },
-    startTime: {
-        type: Number,
-        required: true
-    },
-    duration: {
-        type: Number,
-        required: true,
-        default: 40
-    },
-}, {
-    timestamps: true
-})
+  number: {
+    enum: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ],
+    type: Number,
+    required: true,
+  },
+  startTime: {
+    type: Number,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+    default: 40,
+  },
+});
 
-
-const schoolSchema = new Schema({
+const schoolSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     address: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     city: {
-        type: String,
+      type: String,
     },
     state: {
-        type: String,
+      type: String,
     },
     zip: {
-        type: String,
+      type: String,
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     phone: {
-        type: String,
+      type: String,
     },
-    // teachers: [{
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'Teacher'
-    // }],
-    // students: [{
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'Student'
-    // }],
-    // admins: [{
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'Admin'
-    // }],
-    // classes: [{
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'Class'
-    // }],
-    periods: [PeriodSchema]
-}, {
-    timestamps: true
-})
+    teachers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Teacher",
+      },
+    ],
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Student",
+      },
+    ],
+    admins: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Admin",
+      },
+    ],
+    classes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Class",
+      },
+    ],
+    periods: [PeriodSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default mongoose.model('School', schoolSchema)
+export default mongoose.model("School", schoolSchema);
