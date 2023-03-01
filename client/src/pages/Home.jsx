a5be00126240d58ad882783e37240d7aa5916655
alinaincodeland <alinacuznetov@gmail.com>
2023-03-01 12:50:43 +0100

    Completed the contact form component.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import ContactForm from "../components/ContactForm";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <ContactForm />
+      <Footer />
     </div>
   );
 };
