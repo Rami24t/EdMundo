c9b8560dbf1355046fa6b7b2a5f450e966e3f725
RamS24 <AlSaadi.Rami24@gmail.com>
2023-03-02 15:10:14 +0100

    add routes to taecher

--- server/src/routes/teacher.js
+++ server/src/routes/teacher.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { updateLesson, deleteLesson } from "../controllers/teacher.js";
+import {  updateTeacher, updateLesson, getLessons, getLesson , deleteLesson,
+    getSessions, getSession, getClasses, getClass
+} from "../controllers/teacher.js";
 import auth from "../middleware/auth.js";
 import multerMiddleware from "../config/multer-cloudinary.js";
 const router = express.Router();
@@ -7,16 +9,58 @@ import { check } from "express-validator";
 
 router.put(
     "/lesson",
-    // auth,
+    auth,
     // multerMiddleware.single("image"),
     updateLesson
 );
 
 router.delete(
     "/lesson",
-    // auth,
+    auth,
     deleteLesson
 );
 
-export default router;
+router.get(
+    "/lessons",
+    auth,
+    getLessons
+);
 
+router.get(
+    "/lesson",
+    auth,
+    getLesson
+);
+
+router.get(
+    "/sessions",
+    auth,
+    getSessions
+);
+
+router.get(
+    "/session",
+    auth,
+    getSession
+);
+
+router.get(
+    "/classes",
+    auth,
+    getClasses
+);
+
+router.get(
+    "/class",
+    auth,
+    getClass
+);
+
+router.put(
+    "/teacher",
+    auth,
+    multerMiddleware.single("image"),
+    updateTeacher
+);
+
+export default router;
