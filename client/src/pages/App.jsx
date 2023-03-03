772be81b741b7d512c2aa23ec40166e08b097ba9
RamS24 <AlSaadi.Rami24@gmail.com>
2023-03-03 21:20:16 +0100

    remove footer from login page

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
+  const location = useLocation();
+  const theme = location.pathname;
   return (
     <div>
       <Navbar />
       <Outlet />
-      <Footer />
+     {theme!=='/login' && <Footer />}
     </div>
   );
 };
