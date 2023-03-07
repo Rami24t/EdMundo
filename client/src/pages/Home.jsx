4a3b8288d93101c3f91da1b7b8759731f053e3b2
alinaincodeland <alinacuznetov@gmail.com>
2023-03-07 12:20:27 +0100

    Added hero section on home page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
+import Hero from "../components/Hero";
 import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
+      <Hero />
       <Slider />
       <ContactForm />
     </div>
