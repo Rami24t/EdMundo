5718ebcc28f40d64631382525dfaec17c4350838
RamS24 <AlSaadi.Rami24@gmail.com>
2023-03-13 16:19:44 +0100

    prepare for deployemnt - cors

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://school-online.app"
+        ? "https://ed-mundo.vercel.app/"
         : "http://localhost:3000",
     credentials: true,
     preflightContinue: true,
