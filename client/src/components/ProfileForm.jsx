3475e30f22c9bb5117b09504db7dc4c6a7d5e2c7
RamS24 <AlSaadi.Rami24@gmail.com>
2023-03-15 12:19:28 +0100

    add functionality to student and teacher Profiles

--- client/src/components/ProfileForm.jsx
+++ client/src/components/ProfileForm.jsx
@@ -1,25 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./profileForm.css";
 import { MDBRow, MDBCol, MDBInput, MDBBtn } from "mdb-react-ui-kit";
 import { MDBTypography } from "mdb-react-ui-kit";
 import { useContext } from "react";
 import { Context } from "../components/Context";
+import axios from "axios";
 
 const FormData = {
-  name: "Carol Peletier",
-  email: "peletier_school@school.com",
+  name: "Your Name",
+  email: "youremail@domain.com",
   phone: "1256783746",
   address: "Berlin, Berliner Plz., 1",
-  class: "9a-23",
+  class: "0z-00",
 };
 
 export default function ProfileForm({ role }) {
   const { state, dispatch } = useContext(Context);
-  const profile = {...state.user} || FormData;
-  profile.class = state.user?.currentClass?.name;
-  console.log("profile", state.user);
+  const [profile, setProfile] = useState({...state.user} || FormData);
+  if(state.user.role === "student")
+    profile.class = state.user?.currentClass?.name;
+  // console.log("profile", state.user);
+  const baseUrl = process.env.REACT_APP_BASE_URL;
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProfile({ ...profile, [name]: value });
+  };
 
+  const handleSave = (e) => {
+    e.preventDefault();
+    // backend call /api/users/update
+      axios
+        .put(
+          `${baseUrl}/api/${state.user.role}/update`,
+          { phone: profile.phone, address: profile.address },
+          { withCredentials: true },
+        )
+        .then((res) => {
+          console.log('Save response:', res.data.user);
+          if (res.status === 200) {
+            alert("Profile updated successfully!");            
+          } else if (res.status !== 200) {
+            alert("Profile update failed!");
+          }
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+  };
 
   return (
     <form className="profileForm">
@@ -62,18 +90,22 @@ export default function ProfileForm({ role }) {
       <MDBInput
         wrapperClass="mb-4"
         type="tel"
-        defaultValue={profile?.phone}
+        name="phone"
+        value={profile?.phone}
+        onChange={handleChange}
         id="form6Example6"
         label="Phone"
       />
       <MDBInput
         wrapperClass="mb-4"
         id="form6Example4"
+        name="address"
         label="Address"
-        defaultValue={profile?.address}
+        value={profile?.address}
+        onChange={handleChange}
       />
 
-      <MDBBtn className="mb-4" type="submit" block>
+      <MDBBtn onClick={handleSave} className="mb-4" type="submit" block>
         Save
       </MDBBtn>
     </form>
