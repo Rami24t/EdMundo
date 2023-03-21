27a41b186e74c4f0bbc8ee4c64bc639996b3ed2d
alinaincodeland <alinacuznetov@gmail.com>
2023-03-21 11:53:39 +0100

    Added object proprieties to student lesson card.

--- client/src/components/StudentLessonCard.jsx
+++ client/src/components/StudentLessonCard.jsx
@@ -12,10 +12,7 @@ const StudentLessonCard = () => {
   data = data?.data;
   // console.log("data", data);
   let lessons = data?.user?.lessons || data?.user?.currentClass.lessons;
-  lessons &&
-    lessons.map((lesson, idx) =>
-      console.log("lesson " + (idx + 1) + ": ", lesson),
-    );
+  lessons?.map((lesson, idx) => console.log(`lesson ${idx + 1}: `, lesson));
 
   const lessonDetails = {
     date: "2023-02-02",
@@ -48,158 +45,161 @@ const StudentLessonCard = () => {
 
   return (
     <>
-      <div className="lesson-card">
-        <div className="lesson-header">
-          <div className="title-container">
-            <h2>{lessonDetails.subject}</h2>
+      {" "}
+      {lessons?.map((lesson) => (
+        <div key={lesson._id} className="lesson-card">
+          <div className="lesson-header">
+            <div className="title-container">
+              <h2>{lesson.topic}</h2>
+            </div>
           </div>
-        </div>
-        <hr />
+          <hr />
 
-        <Form>
-          <Row>
-            <FormGroup row>
-              <Col for="date" md={1} className="vertical-center">
-                <BsCalendarDate className="lesson-card-icon" />
+          <Form>
+            <Row>
+              <FormGroup row>
+                <Col for="date" md={1} className="vertical-center">
+                  <BsCalendarDate className="lesson-card-icon" />
+                </Col>
+                <Col md={3} className="date-input">
+                  <Input
+                    className="lesson-card-input"
+                    id="date"
+                    name="date"
+                    type="text"
+                    defaultValue={lesson.date}
+                    disabled
+                  />
+                </Col>
+                <Col for="date" md={1} className="vertical-center">
+                  <TfiTime className="lesson-card-icon" />
+                </Col>
+                <Col md={3} className="date-input">
+                  <Input
+                    className="lesson-card-input"
+                    id="slot"
+                    name="slot"
+                    type="text"
+                    defaultValue={lesson.session}
+                    disabled
+                  />
+                </Col>
+                <Col for="date" md={1} className="vertical-center">
+                  <IoMdPeople className="lesson-card-icon" />
+                </Col>
+                <Col md={3} className="date-input">
+                  <Input
+                    className="lesson-card-input"
+                    id="slot"
+                    name="slot"
+                    type="text"
+                    defaultValue={lesson.teacher}
+                    disabled
+                  />
+                </Col>
+              </FormGroup>
+            </Row>
+
+            <Row>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="topic" className="lesson-card-label">
+                    Topic
+                  </Label>
+                  <Input
+                    className="lesson-card-input"
+                    id="topic"
+                    name="topic"
+                    type="text"
+                    defaultValue={lesson.topic}
+                    disabled
+                  />
+                </FormGroup>
               </Col>
-              <Col md={3} className="date-input">
-                <Input
-                  className="lesson-card-input"
-                  id="date"
-                  name="date"
-                  type="text"
-                  defaultValue={lessonDetails.date}
-                  disabled
-                />
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="link" className="lesson-card-label">
+                    Classroom link
+                  </Label>
+                  <Input
+                    className="lesson-card-input"
+                    id="link"
+                    name="link"
+                    type="url"
+                    defaultValue={lesson.link}
+                    disabled
+                  />
+                </FormGroup>
               </Col>
-              <Col for="date" md={1} className="vertical-center">
-                <TfiTime className="lesson-card-icon" />
-              </Col>
-              <Col md={3} className="date-input">
-                <Input
-                  className="lesson-card-input"
-                  id="slot"
-                  name="slot"
-                  type="text"
-                  defaultValue={lessonDetails.slot}
-                  disabled
-                />
-              </Col>
-              <Col for="date" md={1} className="vertical-center">
-                <IoMdPeople className="lesson-card-icon" />
-              </Col>
-              <Col md={3} className="date-input">
-                <Input
-                  className="lesson-card-input"
-                  id="slot"
-                  name="slot"
-                  type="text"
-                  defaultValue={lessonDetails.teacherName}
-                  disabled
-                />
-              </Col>
+            </Row>
+            <FormGroup>
+              <Label for="objectives" className="lesson-card-label">
+                Objectives
+              </Label>
+              <Input
+                id="objectives"
+                name="objectives"
+                type="text"
+                className="lesson-card-input"
+                defaultValue={lesson.objectives}
+                disabled
+              />
             </FormGroup>
-          </Row>
-
-          <Row>
-            <Col md={6}>
-              <FormGroup>
-                <Label for="topic" className="lesson-card-label">
-                  Topic
-                </Label>
-                <Input
-                  className="lesson-card-input"
-                  id="topic"
-                  name="topic"
-                  type="text"
-                  defaultValue={lessonDetails.topic}
-                  disabled
-                />
-              </FormGroup>
-            </Col>
-            <Col md={6}>
-              <FormGroup>
-                <Label for="link" className="lesson-card-label">
-                  Classroom link
-                </Label>
-                <Input
-                  className="lesson-card-input"
-                  id="link"
-                  name="link"
-                  type="url"
-                  defaultValue={lessonDetails.link}
-                  disabled
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-          <FormGroup>
-            <Label for="objectives" className="lesson-card-label">
-              Objectives
-            </Label>
-            <Input
-              id="objectives"
-              name="objectives"
-              type="text"
-              className="lesson-card-input"
-              defaultValue={lessonDetails.objectives}
-              disabled
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="notes" className="lesson-card-label">
-              Notes
-            </Label>
-            <Input
-              id="notes"
-              name="notes"
-              type="text"
-              className="lesson-card-input"
-              defaultValue={lessonDetails.notes}
-              disabled
-            />
-          </FormGroup>
-          <Row>
-            <Col md={6}>
-              <FormGroup>
-                <Label
-                  for="classwork"
-                  className="vertical-center lesson-card-label "
-                >
-                  Classwork
-                </Label>
-                <Badge
-                  className="lesson-card-badge"
-                  id="classwork"
-                  name="classwork"
-                  target="_blank"
-                  href={lessonDetails.classwork}
-                  defaultValue={lessonDetails.classwork}
-                >
-                  Classwork PDF
-                </Badge>
-              </FormGroup>
-            </Col>
-            <Col md={6}>
-              <FormGroup>
-                <Label for="homework" className="lesson-card-label">
-                  Homework
-                </Label>
-                <Badge
-                  className="lesson-card-badge"
-                  id="homework"
-                  name="homework"
-                  target="_blank"
-                  href={lessonDetails.homework}
-                  defaultValue={lessonDetails.homework}
-                >
-                  Homework PDF
-                </Badge>
-              </FormGroup>
-            </Col>
-          </Row>
-        </Form>
-      </div>
+            <FormGroup>
+              <Label for="notes" className="lesson-card-label">
+                Notes
+              </Label>
+              <Input
+                id="notes"
+                name="notes"
+                type="text"
+                className="lesson-card-input"
+                defaultValue={lesson.materials}
+                disabled
+              />
+            </FormGroup>
+            <Row>
+              <Col md={6}>
+                <FormGroup>
+                  <Label
+                    for="classwork"
+                    className="vertical-center lesson-card-label "
+                  >
+                    Classwork
+                  </Label>
+                  <Badge
+                    className="lesson-card-badge"
+                    id="classwork"
+                    name="classwork"
+                    target="_blank"
+                    href={lesson.classworks}
+                    defaultValue={lesson.classworks}
+                  >
+                    Classwork PDF
+                  </Badge>
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="homework" className="lesson-card-label">
+                    Homework
+                  </Label>
+                  <Badge
+                    className="lesson-card-badge"
+                    id="homework"
+                    name="homework"
+                    target="_blank"
+                    href={lessons.homeworks}
+                    defaultValue={lessons.homeworks}
+                  >
+                    Homework PDF
+                  </Badge>
+                </FormGroup>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      ))}
     </>
   );
 };
