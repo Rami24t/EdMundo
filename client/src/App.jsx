c030696f39882e9da761aeadc71d6e1ad4a63a27
RamS24 <AlSaadi.Rami24@gmail.com>
2023-03-21 17:56:32 +0100

    protect routes fe / and clean code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,26 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { useLocation } from "react-router-dom";
-// import { Context } from "./components/Context";
+import { useLocation, Navigate } from "react-router-dom";
+import { Context } from "./components/Context";
 // import useUser from "./hooks/useUser";
 // import { MDBSpinner } from "mdb-react-ui-kit";
 // import { useSWRConfig } from "swr";
 
 const App = () => {
-  // const { state, dispatch } = useContext(Context);
   const location = useLocation();
   const theme = location.pathname;
-  // const { isLoading, error, data } = useUser();
-  // useEffect(() => {
-  //   if (isLoading) dispatch({ type: "LOADING" });
-  // console.log(' d.e.l.  ', { data, error, isLoading });
-
+  const { state } = useContext(Context);
+  // console.log(state);
   return (
     <div>
       <Navbar />
-      {/* {((theme === "/login" || theme==="/" || theme==='/student' ) || state.user?.name) && <Outlet />} */}
-      <Outlet />
+      {theme === "/login" || theme === "/" || state.user?.name ? (
+        <Outlet />
+      ) : (
+        <Navigate to="/login" />
+      )}
       {theme !== "/login" && <Footer />}
     </div>
   );
