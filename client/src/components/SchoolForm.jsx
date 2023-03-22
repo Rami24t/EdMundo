43c5247acef0114ff6d80dfc5cc16a16e05338df
RamS24 <AlSaadi.Rami24@gmail.com>
2023-03-22 12:52:42 +0100

    add my school  page

---
+++ client/src/components/SchoolForm.jsx
@@ -0,0 +1,131 @@
+import React, { useState, useEffect } from "react";
+import "./profileForm.css";
+import { MDBRow, MDBCol, MDBInput, MDBBtn, MDBSpinner } from "mdb-react-ui-kit";
+import { MDBTypography } from "mdb-react-ui-kit";
+import axios from "axios";
+import useUser from "../hooks/useUser";
+
+const FormData = {
+  name: "Your Name",
+  email: "youremail@domain.com",
+  phone: "1256783746",
+  address: "Berlin, Berliner Plz., 1",
+  class: "0z-00",
+};
+
+export default function SchoolForm() {
+  let { data } = useUser();
+  data = data?.data;
+  const [profile, setProfile] = useState(data?.school);
+
+  useEffect(() => {
+    setProfile((prevProfile) => ({
+      ...prevProfile, ...data.school
+    }));
+  }, [data?.school]);
+
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProfile((prevProfile) => ({ ...prevProfile, [name]: value }));
+  };
+
+  const handleSave = (e) => {
+    e.preventDefault();
+
+    if (data?.user?.role) {
+      axios
+        .put(
+          `${baseUrl}/api/${data?.user?.role}/update`,
+          { phone: profile.phone, address: profile.address },
+          { withCredentials: true },
+        )
+        .then((res) => {
+          console.log("Save response:", res.data.user);
+          if (res.status === 200) {
+            alert("Profile updated successfully!");
+          } else if (res.status !== 200) {
+            alert("Profile update failed!");
+          }
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+  };
+  // if (error) return <div><p>Some error has happened.</p> <p>Please try refreshing your page.</p></div>;
+  if (!data?.user?.name)
+    return (
+      <div>
+        <MDBSpinner grow style={{ width: "3rem", height: "3rem" }}>
+          <span className="visually-hidden">Loading...</span>
+        </MDBSpinner>
+      </div>
+    );
+  return (
+    <form className="profileForm">
+      <MDBTypography variant="h2" className="header-2 font- ">
+        {profile.name}'s Contact Information
+      </MDBTypography>
+      {/* <MDBRow className="mb-4">
+        <MDBCol>
+          <MDBInput
+            value={profile?.name}
+            type="text"
+            label="School"
+            name="name"
+            readOnly
+          />
+        </MDBCol>
+      </MDBRow> */}
+      <MDBInput
+        wrapperClass="mb-4"
+        name="address"
+        label="Address"
+        readOnly
+        value={profile?.address}
+        onChange={handleChange}
+      />
+      <MDBInput
+        wrapperClass="mb-4"
+        type="state"
+        value={profile?.state}
+        label="State"
+        readonly
+        disabled
+      />
+      {data?.user?.role === "student" && (
+        <MDBInput
+          wrapperClass="mb-4"
+          type="text"
+          value={profile?.class}
+          label="Class"
+          readonly
+          disabled
+        />
+      )}
+      <MDBInput
+        wrapperClass="mb-4"
+        type="tel"
+        name="phone"
+        value={profile?.phone}
+        onChange={handleChange}
+        readonly
+        disabled
+        label="Phone"
+      />
+      <MDBInput
+        wrapperClass="mb-4"
+        type="email"
+        value={profile?.email}
+        label="Email"
+        readonly
+        disabled
+      />
+      <MDBBtn onClick={handleSave} className="mb-4" type="submit" block>
+        Save
+      </MDBBtn>
+    </form>
+  );
+}
---
+++ client/src/pages/MySchool.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import SchoolForm from "../components/SchoolForm";
+import student_profile_image from "../images/student_profile_image.png";
+import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
+import { MDBTypography } from "mdb-react-ui-kit";
+
+const MySchool = () => {
+  return (
+    <>
+      <MDBContainer fluid className="p-3 my-5 h-custom">
+        {" "}
+        <header className="text-center">
+          <MDBTypography variant="h1">My School</MDBTypography>
+        </header>
+        <main>
+          <MDBRow
+            className="d-flex  align-items-center m-auto"
+            style={{ maxWidth: "1300px" }}
+          >
+            <MDBCol col="10" md="6" className="d-flex justify-content-center">
+              <img
+                src={student_profile_image}
+                alt="Student Profile Decorative "
+                className="my-5 rounded object-cover"
+                style={{ width: "20rem", height: "20rem" }}
+              />
+            </MDBCol>
+            <MDBCol className="mt-4" col="4" md="6">
+              <div className="  d-flex align-items-center justify-content-center">
+                <SchoolForm role={"student"} />
+              </div>
+            </MDBCol>
+          </MDBRow>
+        </main>
+      </MDBContainer>
+    </>
+  );
+};
+
+export default MySchool;
